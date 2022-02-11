39581ba544679ea0f036da729c89ec20680a14d4
Tibi <andercoiosif@gmail.com>
2022-02-11 20:52:52 +0200

    Fix JWT token refresh after expiration.

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -1,46 +1,40 @@
 import axios from 'axios';
-import {login} from "./actions/authenticated";
-import {connect} from "react-redux";
-
-const mapState = state => {
-  return {
-    authenticated: state.authenticated,
-  }
-};
-
-const mapDispatch = {login};
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_ENDPOINT,
     timeout: 5000,
     headers: {
-        'Authorization': "JWT " + localStorage.getItem('access_token'),
+        'Authorization': "Bearer " + localStorage.getItem('access_token'),
         'Content-Type': 'application/json',
         'accept': 'application/json'
     }
 });
 
-const interceptor = axiosInstance.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response.status !== 401) {
-            return Promise.reject(error);
-        }
-        axiosInstance.interceptors.response.eject(interceptor);
-        const refreshToken = localStorage.getItem('refresh_token');
-        return axiosInstance.post('api/token/refresh/', {
-            refresh: refreshToken,
-        }).then((response) => {
-            const newAccessToken = response.data.access;
-            error.response.config.headers['Authorization'] = "JWT " + newAccessToken;
-            localStorage.setItem('access_token', newAccessToken);
-            return axiosInstance(error.response.config);
-        }).catch(function (error) {
-            console.info(error);
-            this.router.push('/login');
-            return Promise.reject(error);
-        })
+axiosInstance.interceptors.request.use(
+    config => {
+        config.headers['Authorization'] = "Bearer " + localStorage.getItem('access_token');
+        return config;
     }
 )
 
-export default connect(mapState, mapDispatch)(axiosInstance);
+axiosInstance.interceptors.response.use(
+    response => response,
+    async (error) => {
+        let prevRequest = error.config;
+        if (error.response.status === 401 && !prevRequest._retry) {
+            prevRequest._retry = true;
+            let refresh = localStorage.getItem('refresh_token');
+            let newToken = await axiosInstance.post('api/token/refresh/', {refresh}).then(response => {
+                return response.data;
+            });
+
+            localStorage.setItem('access_token', newToken.access);
+            prevRequest.headers['Authorization'] = `Bearer ${newToken.access}`;
+
+            return axiosInstance(prevRequest);
+        }
+        return Promise.reject(error);
+    }
+)
+
+export default axiosInstance;
